90661bf2f8d11f1f87aba8b12deccd76e3332f77
raisasadia <raisa.ummesadia@gmail.com>
2024-05-05 07:37:00 +0600

    home page

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php include('includes/header.php');?>
+
+    <div class="py-5" 
+        style="background-image: url('assets/uploads/products/backgroung.png');
+        background-size:cover;">
+        <div class="container my-5">
+            <div class="row">
+                <div class="col-md-12">
+                    <a href="login.php" class="btn btn-primary mt-4">Login</a>
+                </div>
+            </div>
+        </div>
+    </div>
+
+<?php include('includes/footer.php');?>
+    
